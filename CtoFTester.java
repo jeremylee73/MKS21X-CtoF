52b57460e474471ec5859d0fcd3141768eecc226
Jeremy Lee <jlee00@stuy.edu>
2018-09-19 13:45:52 -0400

    added celsiusToFahrenheit

---
+++ CtoFTester.java
@@ -0,0 +1,9 @@
+public class CtoFTester {
+  public static double celsiusToFahrenheit(double temp) {
+    return ((temp * 1.8) + 32);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(celsiusToFahrenheit(0));
+  }
+}
